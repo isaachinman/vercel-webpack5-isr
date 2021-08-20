c5a24b1ae3d177cf00c4bd6b6a8179b3bf3a93df
Isaac Hinman <isaac@isaachinman.com>
2021-08-20 13:55:05 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+import fs from 'fs'
+import path from 'path'
+
+const Homepage = ({ translations }) => (
+  <div>{translations.hello}</div>
+)
+
+export const getStaticProps = async () => {
+  const locale = 'en'
+  const translationFileExistsInFs = fs.existsSync(
+    path.join(process.cwd(), `locales/${locale}.json`)
+  )
+
+  let translations = undefined
+
+  console.log('Translation file exists in fs: ', translationFileExistsInFs)
+
+  if (translationFileExistsInFs) {
+    translations = (await import(`../locales/${locale}.json`)).default
+  }
+
+  return {
+    revalidate: 10,
+    props: {
+      translations
+    }
+  }
+}
+
+export default Homepage
